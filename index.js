b41a0899c1ee220047a98c5524301476aac9bfde
Sourav Sarkar <samjhonson932@gmail.com>
2024-12-04 13:04:17 +0530

    workasanaBE - 1

---
+++ index.js
@@ -0,0 +1,21 @@
+// backend/server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
